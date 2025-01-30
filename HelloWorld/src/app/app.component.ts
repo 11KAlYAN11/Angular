27222a2497445fb5318fb247294ab5e478293b3f
kalyan <programmer143143@gmail.com>
2025-01-30 17:41:39 +0000

    Added Interpolation & Property Binding

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -8,5 +8,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title = 'Angular Bindings Demo Working Kalyan'; // This will be displayed in the template
+  count = 10; // This will be displayed in the template
+
+  // Property Binding
+  imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  isButtonDisabled = false;
+
+  // Two-Way Binding
+  username = '';
 }

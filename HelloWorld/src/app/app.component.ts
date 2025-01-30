c10fca1dd480ca414c534087e2a6575b4e5ecdf3
kalyan <programmer143143@gmail.com>
2025-01-30 07:16:18 +0000

    Added demo HelloWorld Angular Project

---
+++ HelloWorld/src/app/app.component.ts
@@ -0,0 +1,13 @@
+// All Logic will come into app.componenet.ts
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'HelloWorld';
+}

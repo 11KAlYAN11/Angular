79174c85fa3b6d0f40d2dc90652b92642f7e5d54
kalyan <programmer143143@gmail.com>
2025-02-01 16:46:26 +0000

    Ordered Bindings

--- my-angular16-app/src/app/app.component.ts
+++ my-angular16-app/src/app/app.component.ts
@@ -6,31 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Interpolation: Displaying dynamic text in the template
-  title: string = 'Welcome to Angular 16 Bindings Demo!';
-  subtitle: string = 'Learn how to use Angular bindings effectively.';
+  // Interpolation
+  title: string = 'Angular Bindings Demo';
+  subtitle: string = 'Learn Angular Bindings Easily!';
 
-  // Two-way Data Binding: Binding input fields to properties
+  // Two-way Data Binding
   userInput: string = '';
-  additionalInput: string = '';
+  isChecked: boolean = false;
 
-  // Property Binding: Binding HTML attributes to component properties
+  // Property Binding
   imageUrl: string = 'https://angular.io/assets/images/logos/angular/angular.png';
-  buttonText: string = 'Click Me!';
 
-  // Event Binding: Handling user interactions like button clicks
+  // Event Binding
   onButtonClick() {
-    alert('You clicked the button! User input: ' + this.userInput);
+    alert('Button clicked! User input: ' + this.userInput);
   }
 
-  onAdditionalButtonClick() {
-    alert('Additional button clicked! Input: ' + this.additionalInput);
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    console.log('Input changed:', inputElement.value);
   }
-
-  // Class Binding: Dynamically applying CSS classes
-  isSpecial: boolean = true;
-
-  // Style Binding: Dynamically applying inline styles
-  textColor: string = 'blue';
-  fontSize: string = '20px';
 }
